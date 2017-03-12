c7cb9612f54f2355461b1260d3fdcecc9763cb08
codedork1 <robertcjohnson1984@gmail.comgit config --global user.emailgit config --global user.email robertcjohnson1984@gmail.com>
2017-03-12 13:15:49 -0600

    feat (main.js) Add deleted js items from another file

--- js/main.js
+++ js/main.js
@@ -10,22 +10,60 @@ var commonWords = [
   "could","people","my","than","first","water","been","call",
   "who","oil","its","now","find","long","down","day","did","get",
   "come","made","may","part"
-];
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-var index = getRandomInt(0, commonWords.length);
-var word = commonWords[index];
-console.log (word);
 
-function blanksFromAnswer(answerWord) {
-  var result = "";
-  for(var i= 0; i<answerWord.length; i++) {
-   result = result + "_ "
+// random integer function
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+    var index = getRandomInt(0, commonWords.length);
+    var word = commonWords[index];
+// console.log (word);
+
+
+// making blank spaces function
+    function blanksFromAnswer(answerWord) {
+      var result = "";
+      for(var i= 0; i<answerWord.length; i++) {
+       result = result + "_ "
+      }
+      return result;
+    }
+// console.log (blanksFromAnswer(word));
+
+
+// random word chooser function
+    function findRandomWord () {
+    let number = getRandomInt(0, commonWords.length);
+    .randomWord = commonWords[number];
+    return gameStatus.randomWord
   }
-  return result;
-}
-console.log (blanksFromAnswer(word));
+
+    function makeTracker () {
+    gameStatus.tracker = [];
+    for (var i = 0; i < gameStatus.randomWord.length; i++) {
+      gameStatus.tracker.push(' _');
+    }
+  }
+
+
+    let gameStatus = {
+      randomWord: "",
+      guess: "",
+      tracker: [],
+      feedback: "",
+      guessedLetters: [],
+      mistakes: 8,
+      mistakeMessage: "",
+      head: 'hidden',
+      neck: 'hidden',
+      torso: 'hidden',
+      arm1: 'hidden',
+      arm2: 'hidden',
+      leg1: 'hidden',
+      leg2: 'hidden',
+      hang: 'hidden',
+      btn: 'visible'
+    }

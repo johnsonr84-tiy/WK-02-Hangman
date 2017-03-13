c16b131d27df9994f0258e84ca01bd18a7ae4e64
codedork1 <robertcjohnson1984@gmail.comgit config --global user.emailgit config --global user.email robertcjohnson1984@gmail.com>
2017-03-12 20:27:26 -0600

    feat (mods) Add script to html; Add function calls to main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-var commonWords = [
-  "the","of","and","a","to","in","is","you","that","it","he",
-  "was","for","on","are","as","with","his","they","I","at","be",
-  "this","have","from","or","one","had","by","word","but","not",
-  "what","all","were","we","when","your","can","said","there",
-  "use","an","each","which","she","do","how","their","if","will",
-  "up","other","about","out","many","then","them","these","so",
-  "some","her","would","make","like","him","into","time","has",
-  "look","two","more","write","go","see","number","no","way",
-  "could","people","my","than","first","water","been","call",
-  "who","oil","its","now","find","long","down","day","did","get",
-  "come","made","may","part"
-
 
 // random integer function
     function getRandomInt(min, max) {
@@ -26,7 +13,7 @@ var commonWords = [
 // making blank spaces function
     function blanksFromAnswer(answerWord) {
       var result = "";
-      for(var i= 0; i<answerWord.length; i++) {
+      for (var i= 0; i<answerWord.length; i++) {
        result = result + "_ "
       }
       return result;
@@ -44,9 +31,89 @@ var commonWords = [
     function makeTracker () {
     gameStatus.tracker = [];
     for (var i = 0; i < gameStatus.randomWord.length; i++) {
-      gameStatus.tracker.push(' _');
+      gameStatus.tracker.push(" _");
     }
   }
+
+    function guessTracker () {
+    for(var i = 0; i < gameStatus.guessedLetters.length; i++) {
+      if (' ' + gameStatus.guess === gameStatus.guessedLetters[i]) {
+        gameStatus.feedback = "You have previously selected that letter"
+        show(gameStatus);
+        return false;
+      }
+    }
+      gameStatus.guessedLetters.push(" " + gameStatus.guess);
+  }
+
+  function checkGuess () {
+  if (gameStatus.randomWord.indexOf(gameStatus.guess) === -1) {
+    gameStatus.mistakes -= 1;
+    gameStatus.mistakeMessage = "You have " + gameStatus.mistakes + " selections remaining."
+    gameStatus.feedback = "Try again";
+    if (gameStatus.mistakes > -1) {
+      if (gameStatus.head === "not-visible") {
+        gameStatus.head = "visible";
+      } else if (gameStatus.neck === "not-visible") {
+        gameStatus.neck = "visible";
+      } else if (gameStatus.torso === "not-visible") {
+        gameStatus.torso = "visible";
+      } else if (gameStatus.arm1 === "not-visible") {
+        gameStatus.arm1 = "visible";
+      } else if (gameStatus.arm2 === "not-visible") {
+        gameStatus.arm2 = "visible";
+      } else if (gameStatus.leg1 === "not-visible") {
+        gameStatus.leg1 = "visible";
+      } else if (gameStatus.leg2 === "not-visible") {
+        gameStatus.leg2 = "visible";
+      } else if (gameStatus.hang === "not-visible") {
+          gameStatus.hang = "visible";
+        }
+    }
+
+
+      } else {
+        for (var i = 0; i < gameStatus.randomWord.length; i++) {
+          if (gameStatus.guess === gameStatus.randomWord[i]) {
+            gameStatus.tracker[i] = gameStatus.guess;
+            gameStatus.feedback = "Correct!";
+          }
+        }
+      }
+      show(gameStatus);
+    }
+
+
+    function playerInput () {
+      return document.querySelector("input").value;
+    }
+
+    //Player guess funtions with selection status
+
+    function userGuess() {
+      gameStatus.guess = playerInput();
+      let check = checkGuess();
+      guessTracker();
+      if (gameStatus.tracker.join(" ") === gameStatus.randomWord) {
+        gameStatus.feedback = "Congrats! You win!\nThe correct word is: " + gameStatus.randomWord;
+      }
+      else if (gameStatus.mistakes === 0) {
+        gameStatus.feedback = "Incorrect. You Lose.\nThe correct word is: " + gameStatus.randomWord;
+        gameStatus.btn = "not-visible";
+      }
+      show(gameStatus);
+      return false;
+    }
+
+    function windowOnload () {
+      findRandomWord();
+      makeTracker();
+      show(gameStatus);
+    }
+
+    const reset = () => {
+      location.reload();
+    }
 
 
     let gameStatus = {
@@ -57,13 +124,19 @@ var commonWords = [
       guessedLetters: [],
       mistakes: 8,
       mistakeMessage: "",
-      head: 'hidden',
-      neck: 'hidden',
-      torso: 'hidden',
-      arm1: 'hidden',
-      arm2: 'hidden',
-      leg1: 'hidden',
-      leg2: 'hidden',
-      hang: 'hidden',
-      btn: 'visible'
+      head: "not-visible",
+      neck: "not-visible",
+      torso: "not-visible",
+      arm1: "not-visible",
+      arm2: "not-visible",
+      leg1: "not-visible",
+      leg2: "not-visible",
+      hang: "not-visible",
+      btn: "not-visible",
     }
+
+
+    window.onload = windowOnload();
+
+    document.querySelector(".btn").onclick = userGuess;
+    document.querySelector(".reset").onclick = reset;

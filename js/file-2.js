6055ff575d29001dd2bf3cf362533f66a15133a7
codedork1 <robertcjohnson1984@gmail.comgit config --global user.emailgit config --global user.email robertcjohnson1984@gmail.com>
2017-03-12 20:24:48 -0600

    chore (file) Add second js file with more function calls

---
+++ js/file-2.js
@@ -0,0 +1,15 @@
+function show(gameStatus) {
+  showTracker(gameStatus);
+  showFeedback(gameStatus);
+  showGuessTracker(gameStatus);
+  showMistakes(gameStatus);
+  showHead(gameStatus);
+  showNeck(gameStatus);
+  showTorso(gameStatus);
+  showArm1(gameStatus);
+  showArm2(gameStatus);
+  showLeg1(gameStatus);
+  showLeg2(gameStatus);
+  showHang(gameStatus);
+  showHideButton(gameStatus);
+}
